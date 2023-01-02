8e07a2d2793abe72be071b69bf2c4da52dc04911
Tusu <321756623tu@gmail.com>
2023-01-02 01:00:42 -0800

    created coordinate class and added mobile response

--- 2048.js
+++ 2048.js
@@ -1,11 +1,13 @@
 import Grid from "./Grid.js";
 import Tile from "./Tile.js";
+import Coordinate from "./Coordinate.js";
 
 const gameBoard = document.getElementById("game-board");
 const currentScore = document.getElementById("current-score");
 const bestScore = document.getElementById("best-score");
 const localStorage = window.localStorage;
 var score = 0;
+var touchstartCord, touchendCord;
 
 if (!localStorage.getItem("best-score")) {
   localStorage.setItem("best-score", 0);
@@ -15,9 +17,7 @@ var best = localStorage.getItem("best-score");
 
 bestScore.innerHTML = localStorage.getItem("best-score");
 
-
-console.log(best)
-
+console.log(best);
 
 const grid = new Grid(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
@@ -26,10 +26,12 @@ setupInput();
 
 function setupInput() {
   window.addEventListener("keydown", handleInput, { once: true });
-  window.addEventListener("keydown", handleInput, { once: true });
+  gameBoard.addEventListener("touchstart", handleMobileInput, { once: true });
+  gameBoard.addEventListener("touchend", handleMobileInput, { once: true });
 }
 
 async function handleInput(e) {
+  console.log(e.type);
   console.log(e.key);
   switch (e.key) {
     case "w":
@@ -88,10 +90,10 @@ async function handleInput(e) {
   //check for end state
   if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
     newTile.waitForTransition(true).then(() => {
-          //update best score
-          if (score > best) {
-            localStorage.setItem("best-score", score);
-          }
+      //update best score
+      if (score > best) {
+        localStorage.setItem("best-score", score);
+      }
       alert("You lose");
     });
     return;
@@ -99,65 +101,77 @@ async function handleInput(e) {
   setupInput();
 }
 
-// async function handleMobile(e) {
-//   console.log(e.key);
-//   switch (e.key) {
-//     case "w":
-//     case "ArrowUp": {
-//       if (!canMoveUp()) {
-//         setupInput();
-//         return;
-//       }
-//       await moveUp();
-//       break;
-//     }
-//     case "s":
-//     case "ArrowDown": {
-//       if (!canMoveDown()) {
-//         setupInput();
-//         return;
-//       }
-//       await moveDown();
-//       break;
-//     }
-//     case "a":
-//     case "ArrowLeft": {
-//       if (!canMoveLeft()) {
-//         setupInput();
-//         return;
-//       }
-//       await moveLeft();
-//       break;
-//     }
-//     case "d":
-//     case "ArrowRight": {
-//       if (!canMoveRight()) {
-//         setupInput();
-//         return;
-//       }
-//       await moveRight();
-//       break;
-//     }
-//     default:
-//       setupInput();
-//       return;
-//   }
+async function handleMobileInput(e) {
+  if (e.type === "touchstart") {
+    touchstartCord = new Coordinate(
+      e.changedTouches[0].screenX,
+      e.changedTouches[0].screenY
+    );
+  } else {
+    touchendCord = new Coordinate(
+      e.changedTouches[0].screenX,
+      e.changedTouches[0].screenY
+    );
 
-//   //other code
-//   grid.cells.forEach((cell) => cell.mergeTiles());
+    let deltaX = touchendCord.x - touchstartCord.x;
+    let deltaY = touchendCord.y - touchstartCord.y;
 
-//   const newTile = new Tile(gameBoard);
-//   grid.randomEmptyCell().tile = newTile;
+    console.log(deltaX);
 
-//   //check for end state
-//   if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
-//     newTile.waitForTransition(true).then(() => {
-//       alert("You lose");
-//     });
-//     return;
-//   }
-//   setupInput();
-// }
+    console.log(deltaY / deltaX);
+    if (deltaY < 0 && (deltaY / deltaX > 1 || deltaY / deltaX <= -1)) {
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
+      await moveUp();
+    } else if (deltaY > 0 && (deltaY / deltaX > 1 || deltaY / deltaX <= -1)) {
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
+      await moveDown();
+    } else if (deltaX < 0 && (deltaY / deltaX <= 1 || deltaY / deltaX > -1)) {
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
+      await moveLeft();
+    } else if (deltaX > 0 && (deltaY / deltaX <= 1 || deltaY / deltaX > -1)) {
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
+      await moveRight();
+    }
+
+    console.log(touchendCord);
+    //other code
+    grid.cells.forEach((cell) => {
+      if (cell.mergeTile) {
+        score += cell.mergeTile.value;
+      }
+      cell.mergeTiles();
+      currentScore.innerHTML = score;
+    });
+
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
+
+    //check for end state
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+      newTile.waitForTransition(true).then(() => {
+        //update best score
+        if (score > best) {
+          localStorage.setItem("best-score", score);
+        }
+        alert("You lose");
+      });
+      return;
+    }
+    setupInput();
+  }
+}
 
 function moveUp() {
   sildeTiles(grid.cellsByColumn);
